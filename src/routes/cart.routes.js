b2450881c23aa75a08dc2796048c6b6f0c9a679b
Alvaro Galhardo <alvaromgfernandes@gmail.com>
2022-11-27 14:17:52 -0300

    feat: delete cart

--- src/routes/cart.routes.js
+++ src/routes/cart.routes.js
@@ -3,7 +3,12 @@ import {
   cartExists,
   schemaValidation,
 } from "../middlewares/cart.middlewares.js";
-import { createCart, updateCart, getCart } from "../controllers/cart.js";
+import {
+  createCart,
+  updateCart,
+  getCart,
+  deleteCart,
+} from "../controllers/cart.js";
 import { Router } from "express";
 
 const cartRouter = Router();
@@ -11,5 +16,6 @@ cartRouter.use(authValidation);
 cartRouter.post("/cart/:id", schemaValidation, cartExists, createCart);
 cartRouter.put("/:id", schemaValidation, updateCart);
 cartRouter.get("/cart/:id", getCart);
+cartRouter.delete("/cart/:id", deleteCart);
 
 export default cartRouter;
